d7f62db4a544ea97ac7bccd9bc9e2082353c5779
jzangeva <jzangeva@auto-focus.co.kr>
2024-07-16 15:19:24 +0900

    ~ 50

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # NewLJsp
+
+
+
+
+.48
+
+Controller  => JDBC Service => Data
+service자료형 멤버변수
+
+
+Controller  => <<ingterface>> Serice => JDBC Service => Data
+service자료형 멤버변수
--- src/main/java/com/newlecture/web/controller/IndexController.java
+++ src/main/java/com/newlecture/web/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
 
-        ModelAndView mv = new ModelAndView("index");
+        ModelAndView mv = new ModelAndView("root.index");
         mv.addObject("data", "Hello Spring MVC!!!");
         //mv.setViewName("/WEB-INF/view/index.jsp");
 
